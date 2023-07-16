8def11841ca58ed0dc524f14ba32de7e6cc350fe
keastar <keastar1986@gmail.com>
2023-07-17 01:32:30 +0300

    done 2!
    done 2!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -44,7 +44,7 @@ class App extends Component {
   };
 
   render() {
-    const {contacts, filter} = this.state;
+    const {filter} = this.state;
     const visibleContacts = this.getVisibleContacts();
 
     return (
