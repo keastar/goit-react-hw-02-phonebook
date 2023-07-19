0c2dac483de360b256ebe94c585e36a6606cb099
keastar <keastar1986@gmail.com>
2023-07-19 12:48:16 +0300

    App right!
    App right!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -66,21 +66,6 @@ class App extends Component {
   };
 };
 
-App.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-  filter: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  contacts: PropTypes.arrayOf(
-    PropTypes.exact({
-      id: PropTypes.string.isRequired,
-      number: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      is_complete: PropTypes.bool.isRequired,
-    })
-  ),
-  ondeleteContact: PropTypes.func.isRequired,
-};
-
 export default App;
 
 
